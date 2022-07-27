e280b9d5251510b67df6251614070a0dcc24558b
Hettore Eduardo <duh.barretoo@gmail.com>
2022-07-27 20:04:38 -0300

    Ajustando conexão com banco de dados .

--- src/br/com/agenda/factory/ConnectionFactory.java
+++ src/br/com/agenda/factory/ConnectionFactory.java
@@ -1,5 +1,9 @@
 package br.com.agenda.factory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
 
 	//nome do usuario do mysql
@@ -8,6 +12,34 @@ public class ConnectionFactory {
 	//Senha do banco
 	private static final String PASSWORD = "";
 	
+	//caminho do banco de dados, porta, nome do banco de dados
+	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/agenda";
+	
+	//conexão com o banco de dados 
+
+	public static Connection createConnectionToMySQL() throws Exception {
+		//faz com que a classe seja carregada pela jvm
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		//cria a conexão com o banco de dados
+		Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+		
+		return connection;
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+		
+		//Recuperar a conexão com o banco de dados
+		Connection con = createConnectionToMySQL();
+		
+		//testar se a conexão é nula
+		if(con != null) {
+			System.out.println("Conexão obtida com sucesso!");
+			con.close();
+		}
+		
+	}
 	
 	
 }
