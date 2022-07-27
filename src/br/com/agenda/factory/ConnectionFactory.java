46cbe8eab755a1aadc09f4e18fc7a3c36fc916a3
Hettore Eduardo <duh.barretoo@gmail.com>
2022-07-27 19:44:42 -0300

    Iniciando um crud em Java e mySQL.

---
+++ src/br/com/agenda/factory/ConnectionFactory.java
@@ -0,0 +1,13 @@
+package br.com.agenda.factory;
+
+public class ConnectionFactory {
+
+	//nome do usuario do mysql
+	private static final String USERNAME = "root";
+	
+	//Senha do banco
+	private static final String PASSWORD = "";
+	
+	
+	
+}
