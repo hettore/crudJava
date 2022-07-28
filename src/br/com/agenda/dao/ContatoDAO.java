91b0db1b68701780ca5a0679c515c44f1e2467b8
Hettore Eduardo <duh.barretoo@gmail.com>
2022-07-28 09:17:01 -0300

    Ajustando DAO (Data Access Object).

---
+++ src/br/com/agenda/dao/ContatoDAO.java
@@ -0,0 +1,59 @@
+package br.com.agenda.dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+
+import com.mysql.jdbc.PreparedStatement;
+
+import br.com.agenda.factory.ConnectionFactory;
+import br.com.agenda.model.Contato;
+
+public class ContatoDAO {
+	
+	/*crud
+	c: create
+	r: read
+	u: update
+	d: delete
+	*/
+	
+	public void save(Contato contato) {
+		
+		String sql = "INSERT INTO contatos(nome, idade, datacadastro) VALUES(?, ?, ?)";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		
+		try {
+			//criando uma conexão com o banco de dados
+			conn = ConnectionFactory.createConnectionToMySQL();
+			
+			//criando uma PreparedStatement, para executar uma query
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+			//adicionar os valores q são esperados pela query
+			pstm.setString(1, contato.getNome());
+			pstm.setInt(2,contato.getIdade());
+			pstm.setDate(3, new Date(contato.getDataCadastro().getTime()));
+			
+			//executar a query
+			pstm.execute();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			
+			//fechar as conexões
+			try {
+				if(pstm!=null) {
+					pstm.close();
+				}
+				
+				if(conn!=null) {
+					conn.close();
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
--- src/br/com/agenda/factory/ConnectionFactory.java
+++ src/br/com/agenda/factory/ConnectionFactory.java
@@ -2,7 +2,6 @@ package br.com.agenda.factory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class ConnectionFactory {
 
