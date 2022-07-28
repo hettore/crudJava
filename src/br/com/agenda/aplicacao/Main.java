89ab952b553f9e752329f5caea14a23304a2d69f
Hettore Eduardo <duh.barretoo@gmail.com>
2022-07-28 11:00:51 -0300

    Visualizando os dados do Banco (SELECT).

--- src/br/com/agenda/aplicacao/Main.java
+++ src/br/com/agenda/aplicacao/Main.java
@@ -12,11 +12,17 @@ public class Main {
 		ContatoDAO contatoDao = new ContatoDAO();
 		
 		Contato contato = new Contato();
-		contato.setNome("João Gabriel");
-		contato.setIdade(78);
+		contato.setNome("João Dias");
+		contato.setIdade(75);
 		contato.setDataCadastro(new Date());
 		
 		contatoDao.save(contato);
+		
+		//Visualização dos registros do banco de dados TODOS
+		
+		for(Contato c : contatoDao.getContatos()) {
+			System.out.println("contato: "+ c.getNome());
+		}
 	}
 
 }
--- src/br/com/agenda/dao/ContatoDAO.java
+++ src/br/com/agenda/dao/ContatoDAO.java
@@ -2,6 +2,9 @@ package br.com.agenda.dao;
 
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.mysql.jdbc.PreparedStatement;
 
@@ -9,54 +12,112 @@ import br.com.agenda.factory.ConnectionFactory;
 import br.com.agenda.model.Contato;
 
 public class ContatoDAO {
-	
-	/*crud
-	c: create
-	r: read
-	u: update
-	d: delete
-	*/
-	
+
+	/*
+	 * crud c: create r: read u: update d: delete
+	 */
+
 	public void save(Contato contato) {
-		
+
 		String sql = "INSERT INTO contatos(nome, idade, datacadastro) VALUES(?, ?, ?)";
-		
+
 		Connection conn = null;
 		PreparedStatement pstm = null;
-		
+
 		try {
-			//criando uma conexão com o banco de dados
+			// criando uma conexão com o banco de dados
 			conn = ConnectionFactory.createConnectionToMySQL();
-			
-			//criando uma PreparedStatement, para executar uma query
+
+			// criando uma PreparedStatement, para executar uma query
 			pstm = (PreparedStatement) conn.prepareStatement(sql);
-			//adicionar os valores q são esperados pela query
+			// adicionar os valores q são esperados pela query
 			pstm.setString(1, contato.getNome());
-			pstm.setInt(2,contato.getIdade());
+			pstm.setInt(2, contato.getIdade());
 			pstm.setDate(3, new Date(contato.getDataCadastro().getTime()));
-			
-			//executar a query
+
+			// executar a query
 			pstm.execute();
-			
+
 			System.out.println("Contato salvo com sucesso!");
-			
-		}catch (Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
-			
-			//fechar as conexões
+		} finally {
+
+			// fechar as conexões
 			try {
-				if(pstm!=null) {
+				if (pstm != null) {
 					pstm.close();
 				}
-				
-				if(conn!=null) {
+
+				if (conn != null) {
 					conn.close();
 				}
-			}catch (Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
+	public List<Contato> getContatos(){
+		
+		String sql = "SELECT * FROM contatos";
+		
+		List<Contato> contatos = new ArrayList<Contato>();
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		//Classe que vai recuperar os dados do banco. SELECT
+		ResultSet rset = null;
+		
+		try {
+			conn = ConnectionFactory.createConnectionToMySQL();
+			
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+			
+			rset = pstm.executeQuery();
+			
+			while (rset.next()) {
+				
+				Contato contato = new Contato();
+				
+				//recuperar o id
+				contato.setId(rset.getInt("id"));
+				//recuperar o nome
+				contato.setNome(rset.getString("nome"));
+				//recuperar a idade
+				contato.setIdade(rset.getInt("idade"));
+				//recuperar a data de cadastro
+				contato.setDataCadastro(rset.getDate("datacadastro"));
+				
+				contatos.add(contato);
+				
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				
+				try {
+					if (rset != null) {
+						rset.close();	
+					}
+			
+					if (pstm!=null) {
+						pstm.close();
+					}
+			
+					if(conn!=null) {
+						conn.close();
+					}
+			
+				}catch (Exception e) {
+				e.printStackTrace();
+				}
+			}
+			
+			return contatos;
+		
+	}
 }
+
+		
